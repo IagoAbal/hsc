a686bc6167127225750e0bc9609a5e85ea0e90ec
Iago Abal <iago.abal@gmail.com>
2011-09-05 15:58:07 +0100

    Adds more utilities / smart constructors to H.Prop Ignore-this: 5b15d1ebb6b193104b5ce7a1f765301c

--- H/Prop.hs
+++ H/Prop.hs
@@ -3,9 +3,16 @@ module H.Prop where
 
 import H.Syntax
 
+import Unique
+
 
 _True_ :: Prop p
 _True_ = Con (BuiltinCon TrueCon)
+
+_False_ :: Prop p
+_False_ = Con (BuiltinCon FalseCon)
+
+infixr .==>.
 
 (.&&.), (.||.), (.==>.), (.<=>.) :: Prop p -> Prop p -> Prop p
 p .&&. q = InfixApp p (Op andOp) q
@@ -13,6 +20,8 @@ p .||. q = InfixApp p (Op orOp) q
 p .==>. q = InfixApp p (Op impOp) q
 p .<=>. q = InfixApp p (Op iffOp) q
 
+hypo :: Prop p -> Prop p -> Prop p
+hypo p = (p .==>.)
 
 -- | Splits a proposition into conjunctions
 -- e.g. @splitAnd (p1 && p2 && ... && pN) == [p1,p2,...,pN]@
@@ -24,6 +33,10 @@ splitConj p = [p]
 conj :: [Prop p] -> Prop p
 conj = foldr (.&&.) _True_
 
+hypos :: [Prop p] -> Prop p -> Prop p
+hypos [] p = p
+hypos hs p = hypo (conj hs) p
+
 mkConj :: [Prop p] -> Maybe (Prop p)
 mkConj [] = Nothing
 mkConj ps = Just $ foldr1 (.&&.) ps
@@ -31,3 +44,4 @@ mkConj ps = Just $ foldr1 (.&&.) ps
 
 filterProp :: (Prop p -> Bool) -> Prop p -> Maybe (Prop p)
 filterProp pred = mkConj . filter pred . splitConj
+
