ef478ccf06018668efb0c945924cf5b2fb58efd5
Iago Abal <iago.abal@gmail.com>
2012-04-05 22:02:48 +0100

    Makes Core.Syntax reexport Core.Syntax.FreeVars and Core.Syntax.Subst1

--- Core/Syntax.hs
+++ Core/Syntax.hs
@@ -1,8 +1,12 @@
 
 module Core.Syntax
-  ( module Core.Syntax.AST )
-  where
+  ( module Core.Syntax.AST
+  , module Core.Syntax.FreeVars
+  , module Core.Syntax.Subst1
+  ) where
 
 import Core.Syntax.AST
 import Core.Syntax.Binary()
 import Core.Syntax.Pretty()
+import Core.Syntax.FreeVars
+import Core.Syntax.Subst1
