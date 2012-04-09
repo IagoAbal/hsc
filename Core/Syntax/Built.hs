a1f9a32416b3687a91cdf9925281f8ceb3eb4a0d
Iago Abal <iago.abal@gmail.com>
2012-04-09 18:39:41 +0100

    Add Core.Syntax.Builtin module to contain smart constructors and helpers For now it just defines a 'cleanup' helper, but in future smart constructors from Core.Syntax.AST should be moved to this module.

---
+++ Core/Syntax/Built.hs
@@ -0,0 +1,39 @@
+
+module Core.Syntax.Built where
+
+import Core.Syntax.AST
+import Core.Syntax.FreeVars
+
+import qualified Util.Set as Set
+
+import qualified Data.Generics.Uniplate.Data as G
+import qualified Data.Set as Set
+
+
+cleanup :: Exp -> Exp
+cleanup = G.transform f
+  where f (QP _qt xs p)
+          | Set.fromList xs `Set.disjointWith` fvExp p = p
+        f (QP qt xs (QP qt1 ys p))
+          | qt == qt1 = QP qt (xs ++ ys) p
+        f (InfixApp e1 (OpExp [] (BoolOp OrB)) e2)
+          | e1 == e2      = e1
+          | e1 == mkTrue  = mkTrue
+          | e2 == mkTrue  = mkTrue
+          | e1 == mkFalse = e2
+          | e2 == mkFalse = e1
+        f (InfixApp e1 (OpExp [] (BoolOp AndB)) e2)
+          | e1 == e2 = e1
+          | e1 == mkTrue  = e2
+          | e2 == mkTrue  = e1
+          | e1 == mkFalse = mkFalse
+          | e2 == mkFalse = mkFalse
+        f (InfixApp e1 (OpExp [] (BoolOp ImpB)) e2)
+          | e1 == e2 = mkTrue
+          | e1 == mkTrue  = e2
+          | e2 == mkTrue  = mkTrue
+          | e1 == mkFalse = mkTrue
+          | e2 == mkFalse = mkNot e1
+        f (InfixApp e1 (OpExp [_] (BoolOp EqB)) e2)
+          | e1 == e2 = mkTrue
+        f t = t
