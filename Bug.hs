dfbdb148bca6ec808f19ce50c1548ecdf62bf4d4
Iago Abal <iago.abal@gmail.com>
2012-05-10 12:45:41 +0100

    Add bug-reporting support for 'head' and 'tail' + minor tweaks

--- Bug.hs
+++ Bug.hs
@@ -7,7 +7,7 @@
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- Utils for bug reporting.
+-- Utils for bug reporting, in the spirit of 'Darcs.Bug'.
 -- See also bug.h
 --
 ----------------------------------------------------------------------
@@ -16,10 +16,12 @@ module Bug
   ( _bug
   , _bugDoc
   , _impossible
+  , _head
+  , _tail
   , _fromJust
   ) where
 
-import Pretty ( Doc, errorDoc, text, ($$) )
+import Pretty ( Doc, errorDoc, text, ($$), nest )
 
 type BugStuff = (String, Int, String, String)
 
@@ -28,15 +30,26 @@ _bug bs s = _bugDoc bs (text s)
 
 _bugDoc :: BugStuff -> Doc -> a
 _bugDoc bs s = errorDoc $
-   text ("bug at " ++ _bugLoc bs) $$ s
+   text ("bug at " ++ _bugLoc bs ++ ":")
+    $$ nest 2 s
 
 _bugLoc :: BugStuff -> String
 _bugLoc (file, line, date, time) = file++":"++show line++" compiled "++time++" "++date
 
 _impossible :: BugStuff -> a
-_impossible bs = _bug bs $ "Impossible case at "++_bugLoc bs
+_impossible bs = _bug bs "the impossible happened"
+
+_head :: BugStuff -> [a] -> a
+_head bs l =
+  case l of []    -> _bug bs "head error (empty list)"
+            (x:_) -> x
+
+_tail :: BugStuff -> [a] -> [a]
+_tail bs l =
+  case l of []     -> _bug bs $ "tail error (empty list)"
+            (_:xs) -> xs
 
 _fromJust :: BugStuff -> Maybe a -> a
 _fromJust bs mx =
-  case mx of Nothing -> _bug bs $ "fromJust error at "++_bugLoc bs
+  case mx of Nothing -> _bug bs $ "fromJust error (Nothing)"
              Just x  -> x
--- bug.h
+++ bug.h
@@ -1,8 +1,11 @@
+
 import qualified Bug as Bug_
 
 #define hcBug (\imp_funny_name -> imp_funny_name (__FILE__,__LINE__,__TIME__,__DATE__))
 
 #define bug        (hcBug Bug_._bug)
 #define impossible (hcBug Bug_._impossible)
+#define head       (hcBug Bug_._head)
+#define tail       (hcBug Bug_._tail)
 #define fromJust   (hcBug Bug_._fromJust)
 #define bugDoc     (hcBug Bug_._bugDoc)
